13081ee6c7f61ce16f1c1c4991a9456213af4138
lhp <976181987@qq.com>
2019-03-29 14:10:30 +0800

    123

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,13 @@
 const path = require("path");
 const glob = require("glob");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CSSSplitWebpackPlugin = require('css-split-webpack-plugin').default;
+// const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const CSSSplitWebpackPlugin = require('css-split-webpack-plugin').default;
 const PruifyCSSPlugin = require('purifycss-webpack');
 const webpack = require('webpack');
-
+const pkg = require('./package.json');
 // console.log(path.join(__dirname, '/'))
 
 module.exports = {
@@ -25,6 +26,12 @@ module.exports = {
 
 	},
 	devtool: 'eval-source-map',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			//   Templates: path.resolve(__dirname, 'src')
+		}
+	},
 	//搭建web服务器
 	devServer: {
 		contentBase: path.join(__dirname, '/'), //本地服务器所加载的页面所在的目录
@@ -32,62 +39,115 @@ module.exports = {
 		inline: true, //实时刷新
 		port: 8081, //默认8080
 		host: '0.0.0.0', //访问地址
-		hot: true //热加载
+		hot: true, //热加载
+		// proxy: {
+		// 	'^http://192.168.1.88:8088/uac': {
+		// 		target: 'http://192.168.1.8:8088/uac/',
+		// 		changeOrigin: true,
+		// 		secure: false
+
+		// 	},
+		// 	// '/uac/auth/form': {
+		// 	// 	target: 'http://192.168.1.8:8088',
+		// 	// 	changeOrigin: true,
+		// 	// 	secure: false
+
+		// 	// }
+		// }
 	},
 
 	//添加插件
 	module: {
 		rules: [{
-				test: /(\.jsx|\.js)$/, //兼容react语法
-				use: {
-					loader: "babel-loader"
-				},
-				exclude: /node_modules/
-			}, {
-				test: /\.css$/, //将css嵌入js文件中
-				use: [MiniCssExtractPlugin.loader, {
-					loader: "css-loader?importLoaders=1",
-					options: { //将类名局域化
-						Minimize: true, //代码压缩
-						modules: true, // 指定启用 css modules
-						localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
-					}
-				}, {
-					loader: 'postcss-loader',
-					options: {
-						plugins: [require("autoprefixer")("last 100 versions")]
-					}
-				}],
-				exclude: /src/
-			}, {
-				test: /\.css$/,
-				exclude: /(node_modules)/,
-				loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]'
+			test: /(\.jsx|\.js)$/, //兼容react语法
+			use: {
+				loader: "babel-loader"
 			},
+			exclude: /node_modules/
+		},
 
-			// 小于8K的图片将直接以base64的形式内联在代码中，可以减少一次http请求。
-			// 大于8k的呢?则直接file-loader打包, 这里并没有写明file-loader.但是确实是需要安装,否则会有问题.而name也是file-loader的属性. 重复一次 必须安装file-loader
-			{
-				test: /\.(woff|woff2|eot|ttf|svg|jpg|png|gif)\??.*$/,
-				loader: 'url-loader',
-				query: {
-					// 图片大小限制 单位b
-					limit: 8192,
-					// 生成的文件的存放目录
-					name: 'resourse/[name].[ext]'
-				}
-			}, {
-				test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
-				loader: 'file-loader'
-			}, {
-				test: /\.js$/,
-				loader: 'eslint-loader',
-				enforce: "pre",
-				include: [path.resolve(__dirname, 'src')], // 指定检查的目录
-				options: { // 这里的配置项参数将会被传递到 eslint 的 CLIEngine 
-					formatter: require('eslint-friendly-formatter') // 指定错误报告的格式规范
-				}
+		{
+			test: /\.css$/,
+			exclude: /(node_modules)/,
+			loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]'
+		},
+		{
+			test: /\.less$/,
+			use: [
+				{ loader: 'style-loader' },
+				{ loader: 'css-loader' },
+				{
+					loader: "postcss-loader",
+					options: {
+
+						plugins: [
+							require('autoprefixer')({
+								browsers: ['last 5 version']
+							})
+						]
+					}
+				},
+			
+				{
+					loader: 'less-loader', options: {
+						modifyVars: pkg.theme,
+						javascriptEnabled: true  // 在less里面可以使用JavaScript表达式
+					}
+				},
+			],
+			// 切记这个地方一定要引入antd，文档上没有写入但是一定要因引进去，切记切记
+			include: [/antd/],
+		},
+		{
+			test: /\.less$/,
+			use: [
+				'style-loader',
+				{
+					loader: 'css-loader',
+					options: {
+						modules: true,
+						sourceMap: true,
+						localIdentName: '[name]__[local]_[hash:base64:5]',
+					},
+				},
+				{
+					loader: "postcss-loader",
+					options: {
+						plugins: [
+							require('autoprefixer')({
+								browsers: ['last 5 version']
+							})
+						]
+					}
+				},
+				{ loader: 'less-loader', options: { javascriptEnabled: true } },
+			],
+			exclude: [/antd/],
+		},
+		// 小于8K的图片将直接以base64的形式内联在代码中，可以减少一次http请求。
+		// 大于8k的呢?则直接file-loader打包, 这里并没有写明file-loader.但是确实是需要安装,否则会有问题.而name也是file-loader的属性. 重复一次 必须安装file-loader
+		{
+			test: /\.(woff|woff2|eot|ttf|svg|jpg|png|gif)\??.*$/,
+			loader: 'url-loader',
+			query: {
+				// 图片大小限制 单位b
+				limit: 8192,
+				// 生成的文件的存放目录
+				name: 'resourse/[name].[ext]'
 			}
+		}, {
+			test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+			loader: 'file-loader'
+		}
+			// , {
+			// 	test: /\.js$/,
+			// 	loader: 'eslint-loader',
+			// 	enforce: "pre",
+			// 	include: [path.resolve(__dirname, 'src')], // 指定检查的目录
+			// 	options: { // 这里的配置项参数将会被传递到 eslint 的 CLIEngine 
+			// 		formatter: require('eslint-friendly-formatter') // 指定错误报告的格式规范
+			// 	}
+			// }
 		]
 	},
 	plugins: [
@@ -98,17 +158,32 @@ module.exports = {
 			chunksSortMode: 'none'
 		}),
 		new webpack.HotModuleReplacementPlugin(), //热加载插件
-		new MiniCssExtractPlugin({　　 //分离出css
-			filename: "[name].[chunkhash:8].css",
-			　　chunkFilename: "[id].css"　　
-		}),
-		new CSSSplitWebpackPlugin({ //切割css文件
-			size: 4000,
-			filename: 'css/[name]-[part].[ext]'
-		}),
+		new CopyWebpackPlugin([
+			{
+				"from": "./src/config.test.js",
+				"to": "./config.js"
+			}]),
+		// new MiniCssExtractPlugin({　　 //分离出css
+		// 	filename: "[name].[chunkhash:8].css",
+		// 	chunkFilename: "[id].css"
+		// }),
+		// new CSSSplitWebpackPlugin({ //切割css文件
+		// 	size: 4000,
+		// 	filename: 'css/[name]-[part].[ext]'
+		// }),
 		new PruifyCSSPlugin({ //删除无用的css样式
 			paths: glob.sync(path.join(__dirname, '*.html')) //下所有的html
+		}),
+		new webpack.DefinePlugin({//定义环境变量
+			__DEV__: JSON.stringify(true),
+			__ALLURL__: JSON.stringify({
+				'uac': 'http://192.168.1.8:8082',
+				'indicators': 'http://192.168.31.152:8098'
+
+			}),
+
 		})
 	],
 
 };
+// console.log(process.env.DEBUG)
